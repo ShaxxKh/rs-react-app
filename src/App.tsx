bf60a7af1017aa76c5c15af4a8b0273f68bd2566
Shakhzod Khodjaev <shakhzod.khodjaev@headspace.com>
2025-02-09 00:40:47 +0500

    feat: add basic BrowserRouter with SearchPage and NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { ReactNode } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.css';
 import SearchPage from './pages/SearchPage';
 import ErrorBoundary from './components/ErrorBoundary';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default function App(): ReactNode {
   return (
     <ErrorBoundary>
-      <SearchPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   );
 }
