58b9869a17e8910c2caf9d263167472c3b28b0eb
Shakhzod Khodjaev <shakhzod.khodjaev@headspace.com>
2025-02-09 00:42:11 +0500

    feat: add new components to Results and add needed state and its modification in SearchPage

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,22 +1,70 @@
-import { Person } from '../api/users.api';
+import { useEffect, useState } from 'react';
+import { fetchPersonById, Person, PersonWithoutUrl } from '../api/users.api';
+import Card from './Card';
+import { Outlet, useSearchParams } from 'react-router';
+import DetailedCard from './DetailedCard';
+import Spinner from './Spinner/Spinner';
 
 interface ResultsProps {
   results: Person[];
+  currentPage: number;
+  isNextPage: boolean;
+  onPageChange: (page: number) => void;
 }
 
 export default function Results(props: ResultsProps) {
-  const { results } = props;
+  const [currentCard, setCurrentCard] = useState<PersonWithoutUrl | null>(null);
+  const [isFetchPersonByIdLoading, setIsFetchPersonByIdLoading] =
+    useState(false);
+  const [searchParams] = useSearchParams();
+  const { results, currentPage, isNextPage, onPageChange } = props;
+  const id = searchParams.get('id');
+
+  useEffect(() => {
+    if (id) {
+      setIsFetchPersonByIdLoading(true);
+      fetchPersonById(id)
+        .then(({ name, birth_year, gender, eye_color, hair_color }) => {
+          setCurrentCard({
+            name,
+            birth_year,
+            gender,
+            eye_color,
+            hair_color,
+          });
+        })
+        .finally(() => setIsFetchPersonByIdLoading(false));
+    }
+  }, [id]);
 
   return (
-    <div>
-      <h2>Results</h2>
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>
-            {result.name} - {result.birth_year} - {result.gender}
-          </li>
-        ))}
-      </ul>
+    <div style={{ display: 'flex' }}>
+      <div>
+        <h2>Results</h2>
+        <ul style={{ textAlign: 'start' }}>
+          {results.map((result, index) => (
+            <Card key={index} data={result} />
+          ))}
+        </ul>
+        <button
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage <= 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={!isNextPage}
+        >
+          Next
+        </button>
+      </div>
+      <Outlet />
+      {isFetchPersonByIdLoading ? (
+        <Spinner />
+      ) : (
+        currentCard && <DetailedCard data={currentCard} />
+      )}
     </div>
   );
 }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,58 +2,85 @@ import { useEffect, useState } from 'react';
 import Results from '../components/Results';
 import { fetchPeople, Person } from '../api/users.api';
 import Controls from '../components/Controls';
-import CustomError from '../common/errors/CustomError';
+// import CustomError from '../common/errors/CustomError';
 import Spinner from '../components/Spinner/Spinner';
+import { useSearchParams } from 'react-router';
 
 export default function SearchPage() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem('searchTerm') ?? ''
+  );
   const [results, setResults] = useState<Person[]>([]);
-  const [error, setError] = useState<Error | null>(null);
+  // const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState(false);
+  const [isNextPage, setIsNextPage] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get('page')) || 1;
 
-  async function getPeopleAndUpdateResults(name?: string): Promise<void> {
+  async function getPeopleAndUpdateResults(
+    name?: string,
+    page?: number
+  ): Promise<void> {
     try {
       setLoading(true);
-      const { results } = await fetchPeople(name);
-      setLoading(false);
+      const { results, next } = await fetchPeople(name, page);
+
       setResults(results);
-      setError(null);
+      setIsNextPage(Boolean(next));
+      // setError(null);
     } catch (error) {
-      setError(error as Error);
+      if (error instanceof Error) {
+        // setError(error);
+      }
+
       throw error;
+    } finally {
+      setLoading(false);
     }
   }
 
-  async function handleSearchClick(): Promise<void> {
-    await getPeopleAndUpdateResults(searchTerm);
-  }
-
-  function handleErrorButtonClick() {
-    setError(new CustomError('Error Button Clicked'));
-  }
+  // function handleErrorButtonClick() {
+  //   setError(new CustomError('Error Button Clicked'));
+  // }
 
   function setSearchTermToLocalStorage(searchTerm: string) {
     setSearchTerm(searchTerm);
     localStorage.setItem('searchTerm', searchTerm);
   }
 
-  useEffect(() => {
-    getPeopleAndUpdateResults(searchTerm);
-  }, [searchTerm]);
-
-  if (error) {
-    throw error;
+  function handlePageChange(newPage: number) {
+    searchParams.set('page', newPage.toString());
+    setSearchParams(searchParams);
   }
+
+  useEffect(() => {
+    getPeopleAndUpdateResults(searchTerm, page);
+  }, [searchTerm, page]);
+
+  // if (error) {
+  //   throw error;
+  // }
 
   return (
     <div className="search_page">
       <Controls
         searchTerm={searchTerm}
-        handleSearchClick={handleSearchClick}
+        handleSearchClick={async () =>
+          await getPeopleAndUpdateResults(searchTerm)
+        }
         setSearchTerm={setSearchTermToLocalStorage}
       />
-      {loading ? <Spinner /> : <Results results={results} />}
-      <button onClick={handleErrorButtonClick}>Error Button</button>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Results
+          results={results}
+          currentPage={page}
+          isNextPage={isNextPage}
+          onPageChange={handlePageChange}
+        />
+      )}
+      <button>Error Button</button>
     </div>
   );
 }
