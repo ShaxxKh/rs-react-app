4f51854a35812b9f637962e4852d42c3525fabc9
Shakhzod Khodjaev <shakhzod.khodjaev@headspace.com>
2025-02-09 13:29:29 +0500

    feat: add a message if no cards available

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,11 +2,13 @@ import { Person } from '../api/users.api';
 import Card from './Card';
 
 export default function CardList(props: { results: Person[] }) {
-  return (
+  return props.results.length > 0 ? (
     <ul style={{ textAlign: 'start' }}>
       {props.results.map((result, index) => (
         <Card key={index} data={result} />
       ))}
     </ul>
+  ) : (
+    <h2>No results found</h2>
   );
 }
