32eb6c8026704bd651a4a05036c9fcbf66049325
Shakhzod Khodjaev <shakhzod.khodjaev@headspace.com>
2025-02-19 12:23:07 +0500

    feat: add checkboxe to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,46 @@
 import { Link, useSearchParams } from 'react-router';
 import { Person } from '../api/users.api';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/app/store';
+import {
+  selectIsPersonSelected,
+  setSelectedPerson,
+} from '../features/people/peopleSlice';
+import Checkbox from './Checkbox';
 
 export default function Card(props: { data: Person }) {
   const [searchParams] = useSearchParams();
-  const { name, birth_year, gender, url } = props.data;
-  const urlSplitted = url.split('/');
-  const id = urlSplitted[urlSplitted.length - 2];
-  searchParams.set('id', id);
+  const [cardId, setCardId] = useState(null);
+  const dispatch = useDispatch();
+  const { name, birth_year, gender, hair_color, eye_color, url } = props.data;
+  const person = useSelector((state: RootState) =>
+    selectIsPersonSelected(state, Number(cardId))
+  );
+
+  useEffect(() => {
+    const urlSplitted = url.split('/');
+    const id = urlSplitted[urlSplitted.length - 2];
+    searchParams.set('id', id);
+    setCardId(Number(id));
+  }, [searchParams, url]);
+
+  const handleOnCheckboxChange = () => {
+    dispatch(
+      setSelectedPerson({
+        id: cardId,
+        isSelected: !person?.isSelected,
+        data: { name, birth_year, gender, hair_color, eye_color },
+      })
+    );
+  };
 
   return (
     <li>
+      <Checkbox
+        isChecked={person?.isSelected}
+        handleOnCheckboxChange={handleOnCheckboxChange}
+      />
       <Link to={{ search: searchParams.toString() }}>
         {name} - {birth_year} - {gender}
       </Link>
