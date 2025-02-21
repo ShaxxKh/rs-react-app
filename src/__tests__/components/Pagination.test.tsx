4785d5ffdae8d7632a048549edb62d573a937509
Shakhzod Khodjaev <shakhzod.khodjaev@headspace.com>
2025-02-21 16:54:19 +0500

    refactor: update tests

--- src/__tests__/components/Pagination.test.tsx
+++ src/__tests__/components/Pagination.test.tsx
@@ -1,39 +1,50 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import Results from '../../components/Results';
-import { Person } from '../../api/users.api';
 import { renderWithRouter } from '../helpers';
+import { RootState } from '@/app/store';
 
-const mockResults: Person[] = [
-  {
-    name: 'Luke Skywalker',
-    birth_year: '19BBY',
-    gender: 'male',
-    eye_color: 'yellow',
-    hair_color: 'black',
-    url: 'https://swapi.dev/api/people/1/',
+const mockStore = configureStore([]);
+const initialState: Partial<RootState> = {
+  people: {
+    currentCard: null,
+    isFetchPersonByIdLoading: false,
+    results: [
+      {
+        name: 'Luke Skywalker',
+        birth_year: '19BBY',
+        gender: 'male',
+        eye_color: 'yellow',
+        hair_color: 'black',
+        url: 'https://swapi.dev/api/people/1/',
+      },
+      {
+        name: 'Leia Organa',
+        birth_year: '19BBY',
+        gender: 'female',
+        eye_color: 'yellow',
+        hair_color: 'black',
+        url: 'https://swapi.dev/api/people/2/',
+      },
+    ],
+    searchTerm: '',
+    isFetchPeopleLoading: false,
+    isNextPage: true,
+    selectedPeople: {},
   },
-  {
-    name: 'Leia Organa',
-    birth_year: '19BBY',
-    gender: 'female',
-    eye_color: 'yellow',
-    hair_color: 'black',
-    url: 'https://swapi.dev/api/people/2/',
-  },
-];
+};
 
 test('updates URL query parameter when page changes', async () => {
-  const handlePageChange = jest.fn();
+  const store = mockStore(initialState);
+
   renderWithRouter(
-    <Results
-      results={mockResults}
-      currentPage={1}
-      isNextPage={true}
-      isFetchPeopleLoading={false}
-      onPageChange={handlePageChange}
-    />
+    <Provider store={store}>
+      <Results />
+    </Provider>
   );
+
   await userEvent.click(screen.getByText(/Next/));
-  expect(handlePageChange).toHaveBeenCalledWith(2);
+  expect(window.location.search).toBe('?page=2');
 });
